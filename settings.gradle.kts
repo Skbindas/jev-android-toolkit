2b9ae9a786f580e78aac87092131106df1bdc600
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 09:26:05 +0530

    build: add Gradle project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "jev-android-toolkit"
+
+include(":library:decision-core")
+include(":library:jev-client")
+include(":library:decision-modules")
+include(":sample:compose-playground")
