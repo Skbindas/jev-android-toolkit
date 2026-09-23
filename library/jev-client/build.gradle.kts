90a3dc074937fd50ea9663ac2b1eb0c5030c98fe
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 09:26:13 +0530

    build: add Jev client module

---
+++ library/jev-client/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("maven-publish")
+}
+
+dependencies {
+    api(project(":library:decision-core"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+        }
+    }
+}
