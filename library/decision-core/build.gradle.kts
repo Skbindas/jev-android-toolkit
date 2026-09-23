14f40ec8fe8cc5be5ba489b4b91e31e6a3f8d371
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 09:26:11 +0530

    build: add decision-core module

---
+++ library/decision-core/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("maven-publish")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+        }
+    }
+}
