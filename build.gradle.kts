66af28bba7a869953cfac6c021961c9e10d20724
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 09:26:07 +0530

    build: configure Android and Kotlin plugins

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.application") version "9.0.1" apply false
+    id("com.android.library") version "9.0.1" apply false
+    kotlin("jvm") version "2.2.10" apply false
+    kotlin("android") version "2.2.10" apply false
+    kotlin("plugin.serialization") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+}
+
+group = "dev.skbindas.jev"
+version = "0.1.0"
