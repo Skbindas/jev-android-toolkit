091da24683272f4aec724b2607b04aebe7fbd99b
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 10:08:52 +0530

    build: add Maven publication metadata

--- library/decision-core/build.gradle.kts
+++ library/decision-core/build.gradle.kts
@@ -16,7 +16,24 @@ kotlin {
 publishing {
     publications {
         create<MavenPublication>("release") {
+            artifactId = "decision-core"
             from(components["java"])
+            pom {
+                name.set("Jev Android Toolkit - decision-core")
+                description.set("Core typed Jev decision models and confidence policies.")
+                url.set("https://github.com/Skbindas/jev-android-toolkit")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/Skbindas/jev-android-toolkit")
+                    connection.set("scm:git:https://github.com/Skbindas/jev-android-toolkit.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/Skbindas/jev-android-toolkit.git")
+                }
+            }
         }
     }
 }
