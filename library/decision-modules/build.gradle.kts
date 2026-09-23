0b6dff14c730074214821095241bec1c4728957f
Skbindas <91119501+Skbindas@users.noreply.github.com>
2026-09-23 11:33:36 +0530

    build: prepare signed Central Portal publishing

--- library/decision-modules/build.gradle.kts
+++ library/decision-modules/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.gradle.api.credentials.HttpHeaderAuthentication
+import org.gradle.api.credentials.HttpHeaderCredentials
+
 plugins {
     kotlin("jvm")
     id("maven-publish")
+    id("signing")
 }
 
 dependencies {
@@ -19,11 +23,26 @@ java {
     withJavadocJar()
 }
 
+val centralToken = providers.environmentVariable("CENTRAL_TOKEN").orNull
+
 publishing {
     repositories {
         maven {
             name = "localValidation"
             url = uri(layout.buildDirectory.dir("publishing-validation"))
+        }
+        if (!centralToken.isNullOrBlank()) {
+            maven {
+                name = "centralPortal"
+                url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Authorization"
+                    value = "Bearer $centralToken"
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
         }
     }
 
@@ -41,6 +60,12 @@ publishing {
                         url.set("https://opensource.org/license/mit")
                     }
                 }
+                developers {
+                    developer {
+                        id.set("skbindas")
+                        name.set("Suhaib Choudhary")
+                    }
+                }
                 scm {
                     url.set("https://github.com/Skbindas/jev-android-toolkit")
                     connection.set("scm:git:https://github.com/Skbindas/jev-android-toolkit.git")
@@ -48,5 +73,15 @@ publishing {
                 }
             }
         }
+    }
+}
+
+val signingKey = providers.environmentVariable("MAVEN_CENTRAL_SIGNING_KEY").orNull
+val signingPassword = providers.environmentVariable("MAVEN_CENTRAL_SIGNING_PASSWORD").orNull
+
+signing {
+    if (!signingKey.isNullOrBlank()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["release"])
     }
 }
